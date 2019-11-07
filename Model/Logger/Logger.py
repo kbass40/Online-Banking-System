be506c8aba40d255b6622a7ca4edec2f9b6e173a
Daniel Tymecki <daniel.tymecki@gmail.com>
2019-11-07 15:49:26 -0500

    Set up the basis for Centralized Logger
    - Added new depedency in virtual env to support mysql
    - Created docker-compose to set up mysql database
    - Initiailized The basis for Logging Database
    - Created Logger
    - Wrote Tests to ensure correctness on all added functionality

--- Model/Logger/Logger.py
+++ Model/Logger/Logger.py
@@ -1,2 +1,23 @@
+from Model.Misc import Time as TIME
+from Model.Database import LogDatabase as LD
+
 def printLog():
     print('In logger')
+
+class Logger:
+    def __init__(self):
+        self._db = LD.LogDatabase()
+
+    def get_log_size(self):
+        return self._db.get_size()
+
+    def clear(self,commit=False):
+        self._db.clear_Logs(commit)
+
+    def log_transaction(self,message,commit=True):
+        if not isinstance(message,str):
+            raise TypeError('ERROR: Message must be of type string')
+
+        self._db.insert_into_Logs(TIME.get_timestamp(),'TRANSACTION',message,commit)
+
+        
--- Tests/test_Logger.py
+++ Tests/test_Logger.py
@@ -1,5 +1,17 @@
 from Model.Logger import Logger as LOGGER 
 import pytest
 
+def test_log_transaction_failure():
+    with pytest.raises(TypeError):
+        myLogger = LOGGER.Logger()
+        myLogger.log_transaction(23729)
+
+def test_log_transaction():
+    myLogger = LOGGER.Logger()
+    myLogger.clear()
+    prev = myLogger.get_log_size()
+    myLogger.log_transaction("The is from Logger's test",commit=False)
+    assert prev+1 == myLogger.get_log_size()
+
 def test_print_Logger():
     LOGGER.printLog()
