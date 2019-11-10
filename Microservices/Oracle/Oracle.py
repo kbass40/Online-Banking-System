82524010f25d5f451d13bec24f0427785412214c
Kyle <kylewbassignani@ufl.edu>
2019-11-10 17:41:13 -0500

    added logging

--- Microservices/Oracle/Oracle.py
+++ Microservices/Oracle/Oracle.py
@@ -1,14 +1,40 @@
 from flask import Flask
+from Misc import Time as TIME
 import requests
 import json
 import OracleDB
+from json2html import *
 
 ACCESS_TOKEN = 'Wv62lOHnUq2EYwmmI9DMnfrrznrV'
 SYMBOL = 'ORCL'
 
 app = Flask(__name__)
 
-@app.route("/oracle/get-last", methods=["GET"])
+
+class Logger():
+    def __init__(self):
+        self._db = OracleDB.DBConnection()
+
+    def get_log_size(self):
+        return self._db.get_size()
+
+    def clear(self,commit=False):
+        self._db.clear_Logs(commit)
+
+    def log_transaction(self,message,typing='TRANSACTION',commit=True):
+        if not isinstance(message,str):
+            raise TypeError('ERROR: Message must be of type string')
+
+        if typing not in OracleDB.types:
+            raise ValueError('ERROR: typing must be a defined log type')
+
+        self._db.insert_into_Logs(TIME.get_timestamp(),typing,message,commit)
+
+    def get_logs(self):
+        return self._db.get_logs()
+
+
+@app.route("/api/oracle/get-last", methods=["GET"])
 def get_price():
     response = requests.get('https://sandbox.tradier.com/v1/markets/quotes',
         params={'symbols': (SYMBOL + ',VXX190517P00016000'), 'greeks': 'false'},
@@ -20,7 +46,15 @@ def get_price():
         'description' : json_response['quotes']['quote']['description'],
         'last' : json_response['quotes']['quote']['last']
     }
+    log = Logger()
+    log.log_transaction(('Retrieved stock information: ' + str(ret)), 'INFO')
     return ret
+
+@app.route("/api/oracle/get-logs", methods=["GET"])
+def get_logs():
+    log = Logger()
+    table = log.get_logs()
+    return json2html.convert(json=table)
 
 if __name__ == "__main__" :
     app.run(host="0.0.0.0")
--- Microservices/Oracle/OracleDB.py
+++ Microservices/Oracle/OracleDB.py
@@ -1,11 +1,76 @@
+import mysql.connector
+from Misc import Time as TIME
 
+
+types = ['TRANSACTION','MISC','INFO']
 
 class DatabaseConnection():
 	def foo(self, a): pass
 
 class DBConnection(DatabaseConnection):
-	def foo(self, a):
-		return a
+
+	def __init__(self, user='user',password='password'):
+		self._conn = mysql.connector.connect(
+			host="localhost",
+			user=user,
+			passwd=password,
+			db="db"
+		)
+		self._cursor = self._conn.cursor()
+		self.__initialize__()
+
+	def __enter__(self):
+		return self
+
+	def __exit__(self, exc_type, exc_val, exc_tb):
+		self.commit()
+		self.connection.close()
+
+	def __initialize__(self):
+		self._cursor.execute("CREATE TABLE IF NOT EXISTS db.Logs(time TIMESTAMP, type VARCHAR(20), message TEXT, PRIMARY KEY(time));")
+
+    
+	@property
+	def connection(self):
+		return self._conn
+
+	@property
+	def cursor(self):
+		return self._cursor
+
+	def commit(self):
+		self.connection.commit()
+
+	def insert_into_Logs(self, time, typ, msg, commit = True):
+		if not TIME.is_time_formatted(time):
+			raise TypeError('ERROR time variable must be formatted in the proper format %Y-%m-%d %H:%M:%S')
+
+		if typ not in types:
+			raise ValueError('ERROR typ must be one of the approved types of Logs')
+
+		if not isinstance(msg, str):
+			raise ValueError('ERROR msg must be of type str')
+
+		self._cursor.execute('INSERT INTO db.Logs(time, type, message) VALUES (%s, %s, %s);',(time,typ,msg))
+		if commit:
+			self.commit()
+
+	def get_size(self):
+		self._cursor.execute('SELECT COUNT(*) FROM db.Logs;')
+		row = self._cursor.fetchone()
+		return int(row[0])
+
+	def clear_Logs(self, commit=False):
+		self._cursor.execute('DELETE FROM db.Logs;')
+
+		if commit:
+			self.commit()
+
+	def get_logs(self):
+		self._cursor.execute("SELECT * FROM db.Logs;")
+		ret = self._cursor.fetchall()
+		return ret
+
 
 class TestDBConnection(DatabaseConnection):
 	def foo(self, a):
