05ea41003180484a6a2c77cf076257e65a2ff3a1
Danilo <danilos063@gmail.com>
2019-11-11 05:20:12 -0500

    Commited some source code and htmls for the front end

---
+++ Source/backend.py
@@ -0,0 +1,72 @@
+import os
+import mock_auth, mock_signUp
+from flask import Flask, render_template, request
+
+template_dir = os.path.abspath('../HTML')
+app = Flask(__name__, template_folder=template_dir)
+
+@app.route('/')
+def home():
+    return render_template('home.htm')
+
+#Handles get requests sent to /login
+@app.route('/login')
+def login():
+    #have to manage getting the credentials
+    return render_template('login.htm')
+
+#Handles post requests sent to /login
+@app.route('/login', methods=['POST'])
+def loginPost():
+    #getting the user inputs
+    uname = request.form.get("uname")
+    psw = request.form.get("psw")
+
+    #send it to authentication
+    #currently using a mock
+    authenticated = mock_auth.auth(uname, psw)
+
+    #get session token
+    #currently using a placeholder
+    token ="atrgewa123456678"
+
+    if authenticated:
+        return render_template("successfulLogin.htm").format(token=token)
+
+    else:
+        return render_template("failedLogin.htm")
+
+
+@app.route('/Microservices')
+def mservices():
+    return render_template('mservices.htm')
+
+#Routes for all the web services
+#Ubisoft microservice web page (have to make the html)
+@app.route('/Microservices/Ubisoft')
+def ubi():
+    return render_template('ubi.htm')
+
+@app.route('/SignUp')
+def signUp():
+    return render_template('signUp.htm')
+
+#Handles sign up post requests
+@app.route('/SignUp', methods=['POST'])
+def signUpPost():
+    #getting user inputs
+    uname = request.form.get('uname')
+    email = request.form.get('email')
+    psw = request.form.get('psw')
+
+    #using a mock
+    registered = mock_signUp.signUp(uname, email, psw)
+
+    if registered:
+        return render_template('successfulSignUp.htm')
+    else:
+        return render_template('failedSignUp.htm')
+    
+
+if __name__ == "__main__":
+    app.run()
