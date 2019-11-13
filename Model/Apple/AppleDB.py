68c19ad5c010a6285c5526671e77aa1fc4480ef7
Danny Moolchand <dannyrmoolchand@ufl.edu>
2019-11-13 15:11:07 -0500

    Updated AppleDB and Apple docker-compose to fix

--- Model/Apple/AppleDB.py
+++ Model/Apple/AppleDB.py
@@ -13,12 +13,13 @@ import mysql.connector
 types = ['TRANSACTION','MISC','INFO']
 
 class DatabaseConnection():
-	def __init__(self, user='user',password='password'):
+	def __init__(self, user='root',password='password'):
 		self._conn = mysql.connector.connect(
 			host="localhost",
 			user=user,
 			passwd=password,
-			db="db"
+			db='apple',
+			port='13306'
 		)
 		self._cursor = self._conn.cursor()
 		self.__initialize__()
@@ -31,8 +32,8 @@ class DatabaseConnection():
 		self.connection.close()
 
 	def __initialize__(self):
-		self._cursor.execute("CREATE TABLE IF NOT EXISTS db.Logs(time TIMESTAMP, type VARCHAR(20), message TEXT, PRIMARY KEY(time));")
-		self._cursor.execute("CREATE TABLE IF NOT EXISTS db.Stocks(gainloss DOUBLE, quantity INT);")
+		self._cursor.execute("CREATE TABLE IF NOT EXISTS apple.Logs(time TIMESTAMP, type VARCHAR(20), message TEXT, PRIMARY KEY(time));")
+		self._cursor.execute("CREATE TABLE IF NOT EXISTS apple.Stocks(gainloss DOUBLE, quantity INT);")
 
     
 	@property
@@ -56,7 +57,7 @@ class DatabaseConnection():
 		if not isinstance(msg, str):
 			raise ValueError('ERROR msg must be of type str')
 
-		self._cursor.execute('INSERT INTO db.Logs(time, type, message) VALUES (%s, %s, %s);',(time,typ,msg))
+		self._cursor.execute('INSERT INTO apple.Logs(time, type, message) VALUES (%s, %s, %s);',(time,typ,msg))
 		if commit:
 			self.commit()
 
@@ -65,38 +66,38 @@ class DatabaseConnection():
 			raise ValueError('ERROR gainloss must be of type float')
 		if not isinstance(quantity, int):
 			raise ValueError('ERROR quantity must be of type int')
-		self._cursor.execute('INSERT INTO db.Stocks(gainloss, quantity) VALUES (%s, %s);', (gainloss, quantity))
+		self._cursor.execute('INSERT INTO apple.Stocks(gainloss, quantity) VALUES (%s, %s);', (gainloss, quantity))
 		if commit:
 			self.commit()
 
 	def get_logs_size(self):
-		self._cursor.execute('SELECT COUNT(*) FROM db.Logs;')
+		self._cursor.execute('SELECT COUNT(*) FROM apple.Logs;')
 		row = self._cursor.fetchone()
 		return int(row[0])
 
 	def get_stocks_size(self):
-		self._cursor.execute('SELECT COUNT(*) FROM db.Stocks;')
+		self._cursor.execute('SELECT COUNT(*) FROM apple.Stocks;')
 		row = self._cursor.fetchone()
 		return int(row[0])
 
 	def clear_Logs(self, commit=False):
-		self._cursor.execute('DELETE FROM db.Logs;')
+		self._cursor.execute('DELETE FROM apple.Logs;')
 
 		if commit:
 			self.commit()
 
 	def clear_Stocks(self, commit=False):
-		self._cursor.execute('DELETE FROM db.Stocks;')
+		self._cursor.execute('DELETE FROM apple.Stocks;')
 
 		if commit:
 			self.commit()
 
 	def get_logs(self):
-		self._cursor.execute("SELECT * FROM db.Logs;")
+		self._cursor.execute("SELECT * FROM apple.Logs;")
 		ret = self._cursor.fetchall()
 		return ret
 
 	def get_stocks(self):
-		self._cursor.execute("SELECT * FROM db.Stocks;")
+		self._cursor.execute("SELECT * FROM apple.Stocks;")
 		ret = self._cursor.fetchall()
 		return ret
