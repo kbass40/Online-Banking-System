6fdf582455ad37dbbe74ba24867678ad82e2c411
Danilo <danilos063@gmail.com>
2019-11-14 17:21:08 -0500

    Added support for the sqlite db, fixed an error in test_Facebook.py

--- Tests/test_Ubisoft.py
+++ Tests/test_Ubisoft.py
@@ -1,31 +1,31 @@
-from Model.Facebook import Facebook as FACEBOOK
+from Model.Ubisoft import Ubisoft as UBISOFT
 import pytest
 
 def test_fail_buy_stocks_from_invalid_quantity_type():
     with pytest.raises(TypeError):
-        FACEBOOK.user_buys_stocks("none")
+        UBISOFT.user_buys_stocks(None)
 
 def test_fail_buy_stocks_from_non_int_quantity_type():
     with pytest.raises(TypeError):
-        FACEBOOK.user_buys_stocks("34.15928")
+        UBISOFT.user_buys_stocks("24.444")
 
 def test_fail_buy_stocks_from_raw_int_quantity_type():
     with pytest.raises(TypeError):
-        FACEBOOK.user_buys_stocks(60)
+        UBISOFT.user_buys_stocks(52)
 
 def test_fail_sell_stocks_from_invalid_quantity_type():
     with pytest.raises(TypeError):
-        FACEBOOK.user_sells_stocks("none")
+        UBISOFT.user_sells_stocks(None)
 
 def test_fail_sell_stocks_from_non_int_quantity_type():
     with pytest.raises(TypeError):
-        FACEBOOK.user_sells_stocks("3.14159")
+        UBISOFT.user_sells_stocks("25.99999")
 
 def test_fail_sell_stocks_from_raw_int_quantity_type():
     with pytest.raises(TypeError):
-        FACEBOOK.user_buys_stocks(30)
+        UBISOFT.user_buys_stocks(25)
         
 def test_jsonify():
 	dict = [("-282050.0","5000"), ("-451280.0", "8000")]
-	json = FACEBOOK.jsonify(dict)
+	json = UBISOFT.jsonify(dict)
 	assert json == {1:{"gainloss":"-282050.0","quantity":"5000"},2:{"gainloss":"-451280.0","quantity":"8000"}}
