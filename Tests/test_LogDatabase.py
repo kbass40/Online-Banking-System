f36aa20dc5b2541bb45f5e0dd9504aa031330a4c
Danny Moolchand <dannyrmoolchand@ufl.edu>
2019-11-16 21:21:29 -0500

    Rewrote test_LogDatabase to fit SQLlite queries
    Fixed it so that it can pass on travis_ci

--- Tests/test_LogDatabase.py
+++ Tests/test_LogDatabase.py
@@ -1,39 +1,28 @@
-from Model.Database import LogDatabase as LD
+from Model.Database import MicroserviceDB as MD
 import pytest
-from mysql.connector.errors import ProgrammingError
+#from mysql.connector.errors import ProgrammingError
 from Model.Misc import Time as TIME
 
-def test_LD_default_creation():
-    LD.LogDatabase()
 
-def test_LD_creation_failure():
-    with pytest.raises(ProgrammingError):
-        LD.LogDatabase('user','passwd')
+def test_MD_default_creation():
+    MD.MicroserviceDB('TestDB.sqlite')
 
-def test_LD_insert_time_failure():
-    logDB = LD.LogDatabase()
+def test_MD_insert_time_failure():
+    logDB = MD.MicroserviceDB('TestDB.sqlite')
     with pytest.raises(TypeError):
         logDB.insert_into_Logs('Invalid Time','TRANSACTION','This is a TEST Transaction Message')
 
-def test_LD_insert_type_failure():
-    logDB = LD.LogDatabase()
+def test_MD_insert_type_failure():
+    logDB = MD.MicroserviceDB('TestDB.sqlite')
     with pytest.raises(ValueError):
         logDB.insert_into_Logs(TIME.get_timestamp(),'INVALID TYPE','This is a TEST Transaction Message')
 
-def test_LD_insert_message_failure():
-    logDB = LD.LogDatabase()
+def test_MD_insert_message_failure():
+    logDB = MD.MicroserviceDB('TestDB.sqlite')
     with pytest.raises(TypeError):
         logDB.insert_into_Logs(TIME.get_timestamp(),'TRANSACTION',18)
 
-def test_LD_insert_log():
-    logDB = LD.LogDatabase()
+def test_MD_insert_log():
+    logDB = MD.MicroserviceDB('TestDB.sqlite')
     logDB.insert_into_Logs(TIME.get_timestamp(), 'TRANSACTION', 'This is a TEST Transaction Message')
 
-def test_LD_get_size():
-    logDB = LD.LogDatabase()
-    assert isinstance(logDB.get_size(), int) == True
-
-def test_LD_clear_logs():
-    logDB = LD.LogDatabase()
-    logDB.clear_Logs()
-    assert logDB.get_size() == 0
