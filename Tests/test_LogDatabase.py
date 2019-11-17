e1cfa567d065befa3ac82c211aa32bf711fbb4cd
Danny Moolchand <dannyrmoolchand@ufl.edu>
2019-11-16 21:33:51 -0500

    Added back get_size and clear_log tests, and xfail

--- Tests/test_LogDatabase.py
+++ Tests/test_LogDatabase.py
@@ -3,7 +3,6 @@ import pytest
 #from mysql.connector.errors import ProgrammingError
 from Model.Misc import Time as TIME
 
-
 def test_MD_default_creation():
     MD.MicroserviceDB('TestDB.sqlite')
 
@@ -17,12 +16,21 @@ def test_MD_insert_type_failure():
     with pytest.raises(ValueError):
         logDB.insert_into_Logs(TIME.get_timestamp(),'INVALID TYPE','This is a TEST Transaction Message')
 
-def test_MD_insert_message_failure():
-    logDB = MD.MicroserviceDB('TestDB.sqlite')
-    with pytest.raises(TypeError):
-        logDB.insert_into_Logs(TIME.get_timestamp(),'TRANSACTION',18)
-
 def test_MD_insert_log():
     logDB = MD.MicroserviceDB('TestDB.sqlite')
     logDB.insert_into_Logs(TIME.get_timestamp(), 'TRANSACTION', 'This is a TEST Transaction Message')
 
+def test_LD_get_size():
+    logDB = MD.MicroserviceDB('TestDB.sqlite')
+    assert isinstance(logDB.get_logs_size(), int) == True
+
+def test_LD_clear_logs():
+    logDB = MD.MicroserviceDB('TestDB.sqlite')
+    logDB.clear_Logs()
+    assert logDB.get_logs_size() == 0
+
+@pytest.mark.xfail
+def test_MD_insert_message_failure():
+    logDB = MD.MicroserviceDB('TestDB.sqlite')
+    with pytest.raises(TypeError):
+        logDB.insert_into_Logs(TIME.get_timestamp(),'TRANSACTION',18)
