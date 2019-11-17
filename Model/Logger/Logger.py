a05fa8abd2f1cb6ac13aefb6c72a555606c4ced0
Danny Moolchand <dannyrmoolchand@ufl.edu>
2019-11-16 21:47:42 -0500

    Fixed Logger to use MicroserviceDB and fixed tests

--- Model/Logger/Logger.py
+++ Model/Logger/Logger.py
@@ -1,15 +1,15 @@
 from Model.Misc import Time as TIME
-from Model.Database import LogDatabase as LD
+from Model.Database import MicroserviceDB as MD
 
 def printLog():
     print('In logger')
 
 class Logger:
     def __init__(self):
-        self._db = LD.LogDatabase()
+        self._db = MD.MicroserviceDB('TestDB.sqlite')
 
     def get_log_size(self):
-        return self._db.get_size()
+        return self._db.get_logs_size()
 
     def clear(self,commit=False):
         self._db.clear_Logs(commit)
@@ -18,7 +18,7 @@ class Logger:
         if not isinstance(message,str):
             raise TypeError('ERROR: Message must be of type string')
 
-        if typing not in LD.types:
+        if typing not in MD.types:
             raise ValueError('ERROR: typing must be a defined log type')
 
         self._db.insert_into_Logs(TIME.get_timestamp(),typing,message,commit)
