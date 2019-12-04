1e19e867348bc8c87865380d26587e1786882ed5
Kyle <kylewbassignani@ufl.edu>
2019-12-04 17:06:08 -0500

    buy

--- Model/Microservice/Microservice.py
+++ Model/Microservice/Microservice.py
@@ -49,13 +49,14 @@ def get_price(stock):
         'description' : json_response['quotes']['quote']['description'],
         'last' : json_response['quotes']['quote']['last']
     }
-	# TODO log transaction in log database
-	# db = DB()
-    # db.log_transaction(('Retrieved stock information: ' + str(ret)), 'INFO')
+	
+	auth.push_log(TIME.get_timestamp(), "INFO", stock + " price pull")
+
 	return ret
 
-@app.route('/api/<stock>/buy-stocks=<quantity>/<token>', methods=["GET"])
-def user_buys_stocks(stock, quantity, token=None):
+@app.route('/api/<stock>/buy-stocks=<quantity>/<accountname>/<token>', methods=["GET"])
+def user_buys_stocks(stock, quantity, accountname, token=None):
+	# TODO validate account name
 	if stock not in SYMBOLS:
 		return "stock not found"
 	if token is not None:
@@ -72,8 +73,29 @@ def user_buys_stocks(stock, quantity, token=None):
 	if not quantity.isdigit():
 		raise TypeError('ERROR: Quantity must be of type int')
 
+	price_per_stock = get_price(stock)['last']
+
+	# TODO get bank information from firebase
+	bank_quantity = 
+	bank_gainloss = 
+
+	# check if the bank has enough stocks
+	if bank_quantity < int(quantity):
+		bank_gainloss = bank_gainloss - (price_per_stock * 5000)
+		bank_quantity = bank_quantity + 5000
+	else:
+		bank_gainloss = bank_gainloss + (price_per_stock * int(quantity))
+		bank_quantity = bank_quantity - int(quantity)
+
+	# TODO get function to update the quantity in the bank
+	# auth.update_bank()
+
+	# add stocks to the user account
+	auth.update_user_info(token, accountname, SYMBOLS[stock], quantity, -1 * price_per_stock * quantity)
+
 	auth.push_log(TIME.get_timestamp(), "TRANSACTION", "user buys " + str(quantity) + " " + stock + " stocks")
 
+	# TODO what do we need returned here
 	return "user buys stocks"
 
 @app.route('/api/<stock>/sell-stocks=<quantity>/<token>', methods=["GET"])
@@ -98,6 +120,30 @@ def user_sells_stocks(stock, quantity, token=None):
 
 	return "user sells stocks"
 
+@app.rounte('/api/add_to_balance=<value>/<accountname>/<toke>', methods=["GET"])
+def user_adds_money(value, accountname, token=None):
+	# TODO validate account name
+	if token is not None:
+		try:
+			user = auth._get_userID_from_authID(token)
+			if user is None:
+				return "User not signed in"
+		except:
+			return "Invalid token"
+	else:
+		return "No token"
+	if not isinstance(value,str):
+		raise TypeError('ERROR: value must be of type string')
+	if not value.isdigit():
+		raise TypeError('ERROR: value must be of type int')
+
+	auth.update_user_balance(token, accountname, value)
+
+	auth.push_log(TIME.get_timestamp(), "TRANSACTION", "user added " + str(value) + " to account " + accountname)
+
+	# TODO what to return here
+	return "user adds to balance"
+
 @app.route('/api/get-accounts/<token>', methods=["GET"])
 def get_user_accounts(token=None):
 	if token is not None:
@@ -112,7 +158,7 @@ def get_user_accounts(token=None):
 
 	accounts = auth.get_user_accounts(token)
 	if accounts is None:
-		return "No accounts we messed up"
+		return {}
 	else:
 		return accounts
 
