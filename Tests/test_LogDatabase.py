d5c93a16bb01c32d9abf7101eff4b7fa8e1691b1
Danny Moolchand <dannyrmoolchand@ufl.edu>
2019-12-08 16:24:11 -0500

    Testing Python Stages in travis
    Also fixed logDatabase.py test file

--- Tests/test_LogDatabase.py
+++ Tests/test_LogDatabase.py
@@ -3,19 +3,9 @@ import pytest
 #from mysql.connector.errors import ProgrammingError
 from Model.Misc import Time as TIME
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 def test_MD_default_creation():
     MD.MicroserviceDB('TestDB.sqlite')
 
-=======
-
-=======
->>>>>>> Added back get_size and clear_log tests, and xfail
-def test_MD_default_creation():
-    MD.MicroserviceDB('TestDB.sqlite')
-
->>>>>>> Rewrote test_LogDatabase to fit SQLlite queries
 def test_MD_insert_time_failure():
     logDB = MD.MicroserviceDB('TestDB.sqlite')
     with pytest.raises(TypeError):
@@ -26,25 +16,15 @@ def test_MD_insert_type_failure():
     with pytest.raises(ValueError):
         logDB.insert_into_Logs(TIME.get_timestamp(),'INVALID TYPE','This is a TEST Transaction Message')
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 def test_MD_insert_message_failure():
     logDB = MD.MicroserviceDB('TestDB.sqlite')
     with pytest.raises(TypeError):
         logDB.insert_into_Logs(TIME.get_timestamp(),'TRANSACTION',18)
 
->>>>>>> Rewrote test_LogDatabase to fit SQLlite queries
-=======
->>>>>>> Added back get_size and clear_log tests, and xfail
 def test_MD_insert_log():
     logDB = MD.MicroserviceDB('TestDB.sqlite')
     logDB.insert_into_Logs(TIME.get_timestamp(), 'TRANSACTION', 'This is a TEST Transaction Message')
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> Added back get_size and clear_log tests, and xfail
 def test_LD_get_size():
     logDB = MD.MicroserviceDB('TestDB.sqlite')
     assert isinstance(logDB.get_logs_size(), int) == True
@@ -54,21 +34,10 @@ def test_LD_clear_logs():
     logDB.clear_Logs()
     assert logDB.get_logs_size() == 0
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 #This test is supposed to fail
-=======
->>>>>>> Added back get_size and clear_log tests, and xfail
-=======
-#This test is supposed to fail
->>>>>>> Update
 @pytest.mark.xfail
 def test_MD_insert_message_failure():
     logDB = MD.MicroserviceDB('TestDB.sqlite')
     with pytest.raises(TypeError):
         logDB.insert_into_Logs(TIME.get_timestamp(),'TRANSACTION',18)
-<<<<<<< HEAD
-=======
->>>>>>> Rewrote test_LogDatabase to fit SQLlite queries
-=======
->>>>>>> Added back get_size and clear_log tests, and xfail
