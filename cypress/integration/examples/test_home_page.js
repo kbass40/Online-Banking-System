7a6d904ed57c800b91a30689036aacd8f1d9f2e0
Danny Moolchand <dannyrmoolchand@ufl.edu>
2019-12-08 22:27:13 -0500

    Delete default cypress examples and added our js tests

---
+++ cypress/integration/examples/test_home_page.js
@@ -0,0 +1,11 @@
+describe('The Home Page', function() 
+{
+    it('successfully loads', function() {
+      cy.visit('http://localhost:5000/SignUp') // change URL to match your dev URL
+      cy.get('[placeholder="Username"]').type('test_cypress')
+      cy.get('[placeholder="Email"]').type('testcypress@company.com')
+      cy.get('[type="password"]').type('12345678')
+      cy.get('button').click()
+      cy.get('h2').should('have.value', 'Sign up failed')
+    });
+});
