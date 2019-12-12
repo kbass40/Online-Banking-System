a25d336b70cdc8b9a1f8c756264b5d39bdba2a6e
Danilo <danilos063@gmail.com>
2019-12-12 13:48:27 -0500

    Added basnk stock info to admin dashboard

--- Model/WebServer/admin_backend.py
+++ Model/WebServer/admin_backend.py
@@ -1,14 +1,19 @@
 import os
 import sys
-sys.path.append(os.path.abspath(".."))
+from pathlib import Path
+# Both parent directories need to be added to function from top-level as well as from local 
+path = Path(__file__).parent.absolute()
+sys.path.append(str(path) + '//..')
+sys.path.append(str(path) + '//..//..')
+
 from Database import AuthenticationDatabase as authdb
 import mock_adminAuthentication
 from flask import Flask, render_template, request, redirect, url_for
 from json2html import *
 from requests import HTTPError
 
-template_dir = os.path.abspath('../HTML')
-app = Flask(__name__, template_folder=template_dir)
+template_dir = str(path) + '//..//HTML'
+app = Flask(__name__, template_folder=template_dir, static_folder=template_dir + "/static")
 
 #home directory
 @app.route('/')
@@ -35,14 +40,28 @@ def admin_home_auth():
     except Exception as e:
         return render_template("unauthenticated_admin.htm").format(error=str(e))
 
+    bank_info = []
+
+    try:
+        for i in authdb.stock_symbols:
+            bank_info.append(authdb.AuthDatabase().get_bank_info(i))
+    except HTTPError:
+        print("Oops something didn't go as planned")
+
     #creating a placeholder token and setting it as a cookie
-    response = app.make_response(render_template('admin_dashboard.htm'))
+    response = app.make_response(render_template('admin_dashboard.htm', 
+    apple_amount=bank_info[0]['stock_num'], apple_gain_loss=bank_info[0]['gain-loss'],
+    fcb_amount=bank_info[1]['stock_num'], fcb_gain_loss=bank_info[1]['gain-loss'],
+    google_amount=bank_info[2]['stock_num'], google_gain_loss=bank_info[2]['gain-loss'],
+    oracle_amount=bank_info[3]['stock_num'], oracle_gain_loss=bank_info[3]['gain-loss'],
+    ubi_amount=bank_info[4]['stock_num'], ubi_gain_loss=bank_info[4]['gain-loss']))
+
     response.set_cookie("authenticated", value=token)
     return response
 
 
 #Print logs
-@app.route('/admin_dashboard')
+@app.route('/logs')
 def print_logs():
     #getting user token
     token = request.cookies.get('authenticated')
@@ -53,8 +72,20 @@ def print_logs():
         for i,key in enumerate(logs):
             stripped_logs[i] = logs[key]
         return json2html.convert(json=stripped_logs)
-    except HTTPError as e:
+    except HTTPError:
         return 'Logs cannot be printed at this time.'
+
+#get bank balace
+@app.route('/balance')
+def bank_stocks():
+    bank_info = []
+
+    try:
+        for i in authdb.stock_symbols:
+            bank_info.append(authdb.AuthDatabase().get_bank_info(i))
+    except HTTPError:
+        return 'Something went wrong with the operation'
+
     
 
 
