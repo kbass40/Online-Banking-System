d198f892792e4e0c0b4f9bcda32579fcb4748eb6
Danilo <danilos063@gmail.com>
2019-12-12 16:14:23 -0500

    Created Admin login and dashboard cypress tests

---
+++ cypress/integration/Admin_Login/test_failed_admin_login.js
@@ -0,0 +1,9 @@
+describe('Failed Admin login', function(){
+    it('Tests the admin login page for a failed login', function(){
+        cy.visit("http://localhost:5555/admin")
+        cy.get('[type="text"]').type('admin@admin.com')
+        cy.get('[type="password"]').type('12345678')
+        cy.get('button').click()
+        cy.get('h2').contains('Log in failed')
+    });
+});
