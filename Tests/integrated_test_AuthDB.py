16d1481a5a93803f0b232f0dc9bce0b267a4f61d
Daniel Tymecki <daniel.tymecki@gmail.com>
2019-12-12 16:29:38 -0500

    Fixed Bug Where Users would be deleted if explicitly created

--- Tests/integrated_test_AuthDB.py
+++ Tests/integrated_test_AuthDB.py
@@ -13,17 +13,21 @@ import random
 def test_create_more_than_three_accounts_for_single_user():
     r = str(random.randint(1,100))
     myDb = AuthDB.AuthDatabase()
+    myDb.delete_all_random_kyles()
     myDb.create_new_user('kyle'+r+'@email.com','password123')
     auth_id = myDb.authenticate_user_via_email_password('kyle'+r+'@email.com','password123')
     myDb.create_new_account_for_user(auth_id,'Account v1')
+    myDb.update_user_balance(auth_id,'Account v1',5000.00)
     myDb.create_new_account_for_user(auth_id,'Account v2')
-    myDb.create_new_account_for_user(auth_id,'Account v3')
 
     try:
-        myDb.create_new_account_for_user(auth_id,'Account v4')
+        myDb.create_new_account_for_user(auth_id,'Account v3')
     except RuntimeError:
         print('Stopped adding an invalid number of accounts')
     except:
         print("Something else went wrong :(")
-    finally:
-        myDb.delete_autheticated_user_from_auth_id(auth_id)
+    
+    myDb.delete_autheticated_user_from_auth_id(auth_id)
+
+
+test_create_more_than_three_accounts_for_single_user()
