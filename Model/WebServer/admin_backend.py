c308c4c8f46273e2fc58dea47df5d1fe275788eb
tim crowley <tcrowley@ufl.edu>
2019-12-12 11:44:10 -0500

    linking admin dashboard

--- Model/WebServer/admin_backend.py
+++ Model/WebServer/admin_backend.py
@@ -33,13 +33,14 @@ def admin_home_auth():
 
     #creating a placeholder token and setting it as a cookie
     token = "abheo457612n"
-    response = app.make_response(render_template('authenticated_admin.htm'))
+    response = app.make_response(render_template('admin_dashboard.htm'))
     response.set_cookie("authenticated", value=token)
     return response
 
-#Authentication Logs
-@app.route('/auth-logs')
-def auth_logs():
+
+#Print logs
+@app.route('/admin_dashboard')
+def print_logs():
     #getting user token
     token = request.cookies.get('authenticated')
 
@@ -51,45 +52,37 @@ def auth_logs():
 
     if valid:
         #have to work with database or another function to get all the logs
-        return 'Logs'
+    return json2html.convert(json=requests.get("http://localhost:8000/api/admin_dashboard").text)
     else:
-        return 'Log in to access this functionality'
+        return 'Logs cannot be printed at this time.'
 
 
-#Have to change functionality for the next two
-#Transaction Logs
-@app.route('/transact-logs')
-def transact_logs():
-    #getting user token
-    token = request.cookies.get('authenticated')
+#Handles post requests sent to /login
+@app.route('/admin_login', methods=['POST'])
+def adminLogin():
+    #getting the user inputs
+    email = request.form.get("email")
+    psw = str(request.form.get("psw"))
 
-    valid = False
+    if ((email == "admin@admin.com") && (psw == "admin1"))
+    #send it to authentication
+    #authenticated = mock_auth.auth(uname, psw)
 
-    #have to find a way to validate token, set to True for now
-    if token is not None:
-        valid = True
+    #get session token
+    temp = authdb.AuthDatabase()
 
-    if valid:
-        return 'Logs'
-    else:
-        return 'Log in to access this functionality'
+    try:
+        token = temp.authenticate_user_via_email_password(email, psw)
+    except Exception as e:
+        return render_template("failedLogin.htm").format(error=str(e))
 
-#Stock Transaction
-@app.route('/stock-transact')
-def stock_transact():
-    #getting user token
-    token = request.cookies.get('authenticated')
+    response = app.make_response(redirect(url_for("admin_dashboard")))
+    response.set_cookie("admin", value=token)
 
-    valid = False
+    print(token)
 
-    #have to find a way to validate token, set to True for now
-    if token is not None:
-        valid = True
-
-    if valid:
-        return 'Logs'
-    else:
-        return 'Log in to access this functionality'
+    return response
+    
 
 
 if __name__=="__main__":
